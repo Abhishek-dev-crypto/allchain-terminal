44b9c038c8e1e683b33abafe805bc123db902e92
Abhishek-dev-crypto <abhiii31@gmail.com>
2026-06-03 14:30:08 +0530

    start button

--- app/page.tsx
+++ app/page.tsx
@@ -401,9 +401,15 @@ useEffect(() => {
       <span className="text-lg font-semibold">AllChain</span>
     </div>
 
-    <button
+   <button
   onClick={login}
-  className="px-4 sm:px-5 py-2 bg-white text-black text-xs sm:text-sm font-semibold rounded-lg hover:scale-105 transition whitespace-nowrap"
+  className="px-4 sm:px-5 py-2 text-xs sm:text-sm font-semibold rounded-lg hover:scale-105 transition whitespace-nowrap"
+  style={{
+    backgroundColor: "#ffffff",
+    color: "#000000",
+    WebkitAppearance: "none",
+    appearance: "none",
+  }}
 >
       Start Trading
     </button>

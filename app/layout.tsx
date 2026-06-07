79a9506481d4c84597eee22a0da0758043962f84
Abhishek-dev-crypto <abhiii31@gmail.com>
2026-06-07 19:42:08 +0530

    google analytics install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import './globals.css';
+import "./globals.css";
 import { usePathname } from "next/navigation";
+import Script from "next/script";
 
 import { NotificationProvider } from "./contexts/NotificationContext";
 import ClientWrapper from "./components/ClientWrapper";
 import Warning from "./components/Warning";
-import NavBar from './components/Navigation';
+import NavBar from "./components/Navigation";
 
 export default function RootLayout({
   children,
@@ -17,21 +18,33 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-W3ERYPVTN7"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-W3ERYPVTN7');
+        `}
+      </Script>
+
       <body>
-  <NotificationProvider>
+        <NotificationProvider>
+          {pathname !== "/" && <Warning />}
 
-    {pathname !== "/" && <Warning />}
+          <ClientWrapper>{children}</ClientWrapper>
 
-    
-
-    <ClientWrapper>{children}</ClientWrapper>
-
-    <div className="border-t border-black bg-yellow-300 px-4 py-2 text-center text-[10px] font-extrabold tracking-widest text-black">
-      SIMULATION MODE ACTIVE — ALLCHAIN CURRENTLY OPERATES IN A PAPER TRADING ENVIRONMENT
-    </div>
-
-  </NotificationProvider>
-</body>
+          <div className="border-t border-black bg-yellow-300 px-4 py-2 text-center text-[10px] font-extrabold tracking-widest text-black">
+            SIMULATION MODE ACTIVE — ALLCHAIN CURRENTLY OPERATES IN A PAPER
+            TRADING ENVIRONMENT
+          </div>
+        </NotificationProvider>
+      </body>
     </html>
   );
 }
